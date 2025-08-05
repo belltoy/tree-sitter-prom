56e45e6dae753e1dadce76d58ed67ea9333afa34
belltoy <belltoy@gmail.com>
2025-08-05 15:49:12 +0800

    Add highlight query

---
+++ queries/highlights.scm
@@ -0,0 +1,22 @@
+[
+  (comment)
+  (type_line)
+  (help_line)
+] @comment
+
+[
+  "TYPE"
+  "HELP"
+] @keyword
+
+(metric_type) @type
+
+(metric_name) @variable
+
+(label_name) @label
+
+(label_value) @string
+
+(metric_value) @number.float
+
+(timestamp) @number
